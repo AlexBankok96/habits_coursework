c0a2eca515e829f90fbddd6fc80c88ce7cea17ac
AlexBankok96 <Sergienko_15@mail.ru>
2025-01-07 17:00:16 +0300

    Update validators.py

--- habits/validators.py
+++ habits/validators.py
@@ -1,21 +1,30 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
-class HabitsValidator:
-    def __init__(self, field):
-        self.field = field
+def validate_compensation(value):
+    """Исключает одновременный выбор связанной привычки и указания вознаграждения."""
+    reward = dict(value).get("reward", "Exists")
+    associated_habit = dict(value).get("associated_habit", "Exists")
+    if reward and associated_habit:
+        raise ValidationError(
+            "Нельзя выбирать приятную привычку и вознаграждение одновременно"
+        )
 
-    def __call__(self, value):
-        if value['is_good']:
-            if value['related'] or value['prize']:
-                raise serializers.ValidationError(
-                    'У приятной привычки не может быть связанной привычки или вознаграждения')
-            if value['related'] and value['prize']:
-                raise serializers.ValidationError(
-                    'Может быть связанная привычка или вознаграждение,')
-            if value['duration'] > 120:
-                raise serializers.ValidationError(
-                    'Длительность привычки не может быть больше 2 минут')
-            if value['related']:
-                if not value['related'].is_good:
-                    raise serializers.ValidationError('Связанные привычки = приятные привычки')
+
+def validate_associated_habit(value):
+    """Проверяет, что в связанные привычки могут попадать только привычки с признаком приятной привычки."""
+    associated_habit = dict(value).get("associated_habit")
+    if associated_habit and not associated_habit.is_pleasant:
+        raise ValidationError("Связанная привычка должна быть приятной")
+
+
+def validate_pleasant_habit(value):
+    """Проверяет что у приятной привычки нет вознаграждения или связанной привычки."""
+    is_pleasant = dict(value).get("is_pleasant", None)
+    reward = dict(value).get("reward", None)
+    associated_habit = dict(value).get("associated_habit", None)
+    if is_pleasant and (associated_habit or reward):
+        raise ValidationError(
+            "Приятная привычка не может быть связана с другой приятной привычкой или вознаграждением"
+        )
